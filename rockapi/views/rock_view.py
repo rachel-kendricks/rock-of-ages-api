82b90f1b8abd172b166ed4f195ea836f60400337
Rachel Kendricks <rachelanne.kendricks@gmail.com>
2024-03-20 13:59:41 -0500

    expanded user and type of rock

--- rockapi/views/rock_view.py
+++ rockapi/views/rock_view.py
@@ -2,7 +2,8 @@ from django.http import HttpResponseServerError
 from rest_framework import serializers, status
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
-from rockapi.models import Rock
+from rockapi.models import Rock, Type
+from django.contrib.auth.models import User
 
 
 class RockView(ViewSet):
@@ -32,8 +33,27 @@ class RockView(ViewSet):
             return HttpResponseServerError(ex)
 
 
+class RockOwnerSerializer(serializers.ModelSerializer):
+    """JSON serializer"""
+
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name")
+
+
+class RockTypeSerializer(serializers.ModelSerializer):
+    """JSON serializer"""
+
+    class Meta:
+        model = Type
+        fields = ("label",)
+
+
 class RockSerializer(serializers.ModelSerializer):
     """JSON serializer"""
+
+    type = RockTypeSerializer(many=False)
+    user = RockOwnerSerializer(many=False)
 
     class Meta:
         model = Rock
@@ -41,4 +61,6 @@ class RockSerializer(serializers.ModelSerializer):
             "id",
             "name",
             "weight",
+            "user",
+            "type",
         )
