c67bb3ffb03dad4ff1078974ee60f7be28e3eb9f
Rachel Kendricks <rachelanne.kendricks@gmail.com>
2024-03-21 10:35:18 -0500

    destroy added to rock_view

--- rockapi/views/rock_view.py
+++ rockapi/views/rock_view.py
@@ -10,14 +10,26 @@ class RockView(ViewSet):
     """Rock view set"""
 
     def create(self, request):
-        """Handle POST operations
+        """Handle POST requests for rocks
 
         Returns:
-            Response -- JSON serialized instance
+            Response: JSON serialized representation of newly created rock
         """
 
-        # You will implement this feature in a future chapter
-        return Response("", status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        # Get an object instance of a rock type
+        chosen_type = Type.objects.get(pk=request.data["type_id"])
+
+        # Create a rock object and assign it property values
+        rock = Rock()
+        rock.user = request.auth.user
+        rock.weight = request.data["weight"]
+        rock.name = request.data["name"]
+        rock.type = chosen_type
+        rock.save()
+
+        serialized = RockSerializer(rock, many=False)
+
+        return Response(serialized.data, status=status.HTTP_201_CREATED)
 
     def list(self, request):
         """Handle GET requests for all items
@@ -31,6 +43,25 @@ class RockView(ViewSet):
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as ex:
             return HttpResponseServerError(ex)
+
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single item
+
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            void = Void.objects.get(pk=pk)
+            void.delete()
+            return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+        except Void.DoesNotExist as ex:
+            return Response({"message": ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response(
+                {"message": ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
 
 
 class RockOwnerSerializer(serializers.ModelSerializer):
